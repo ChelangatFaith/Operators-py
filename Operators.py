6caef984fe55dd639e80c8b8239fcf2479e7101b
SamanthaFaith <115935667+ChelangatFaith@users.noreply.github.com>
2023-01-19 14:36:21 +0300

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,34 @@
+#In python operators are used in doing comparisons between numbers
+#or rather testing the conditions
+#Assignment operator 
+#They involve(=,+=,++,/=)
+
+Marks = 100
+Marks += 2
+print(Marks)
+
+#Comparison operators
+#(==,===,!=,>,<, )
+#Used to check the values on the left are equal to 
+#the right one
+#The output is either true or false
+
+a=2
+b=3
+print(a==b)
+print(a<b) 
+print(a>b)
+print(a!=b)
+
+#Logial operators
+#(and,or,NOT) 
+#AND,Both conditions must be true 
+#OR,One of the conditions is true
+#NOT,flip the truth or rather negate the statement
+a =20
+b =10
+c = "Dam"
+
+if a>=10 or c ==("House"):
+    print("hi")
+
